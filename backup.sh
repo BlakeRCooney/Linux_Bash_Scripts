f5238e1b1717d0bf1dc17f3530b874ff6bc2eb4c
BlakeRCooney <105319499+BlakeRCooney@users.noreply.github.com>
2022-10-17 19:58:54 -0400

    Update backup.sh
    Added comments to the file

--- backup.sh
+++ backup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Author: Blake Cooney
+# Date Created: 09/29/2022
+# Last Modified: 09/29/2022
+
+# Description:
+# Creates a backup in ~/backups folder of all files in the home directory
+
+# Usage:
+# backup.sh
+
 tar -czf ~/backups/backup.tar.gz ~/{Documents,Downloads,Desktop,Pictures,Videos} 2>/dev/null
 date >> ~/backups/backups.log
