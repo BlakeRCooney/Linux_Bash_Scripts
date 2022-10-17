36a0cb13dbc6c78f3ce93899f08a73223f6e681e
BlakeRCooney <105319499+BlakeRCooney@users.noreply.github.com>
2022-10-17 19:33:44 -0400

    Update bluetooth.sh
    Added descriptions on how to use this bash script

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Author: Blake Cooney
+# Date Created: 09/12/2022
+# Last Modified: 10/17/2022
+
+# Discription
+# Use this script to allow bluetooth to work on your Linux System
+
+# Usage
+# bash bluetooth.sh
+
 sudo modprob -r btusb
 sudo modprob btusb
 sudo reboot
+
